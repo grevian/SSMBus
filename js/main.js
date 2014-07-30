f127553b32e1eb49a307e457053f16fe2da004f0
Joshua Hayes-Sheen <grevian@gmail.com>
2014-07-30 04:28:45 +0000

    init

---
+++ js/main.js
@@ -0,0 +1,31 @@
+$(function () {
+if (geoPosition.init()) {
+    geoPosition.getCurrentPosition(
+      success_callback,
+      error_callback,
+      {
+        enableHighAccuracy:true
+      });
+  }
+  else
+  {
+    // Cannot use geoposition, Pop up a warning/error toast, maybe allow map/stop browsing as a fallback
+    
+  }
+  
+  // p : geolocation object
+  function success_callback(p){
+        // p.latitude : latitude value
+        // p.longitude : longitude value
+        $.get('/find/nearest', {position: JSON.stringify(p)}, displayNearest);
+    }
+
+    function error_callback(p){
+        // p.message : error message
+    }  
+});
+
+function displayNearest(data) {
+  // Zoom to the current position, or nearest bus stop on the map,
+  // Highlight the nearby bus stops in the list, provide basic info on them
+}
